364744073c16664a703e5e6af5bf880bccd9810a
eccentricharry <harinathrao13@gmail.com>
2025-11-21 10:28:45 +0530

    useEffect, useState

--- React/counter/src/App.jsx
+++ React/counter/src/App.jsx
@@ -1,11 +1,38 @@
+import React, {useEffect, useState} from "react";
 import './App.css'
 
 function App() {
+    const [isVisible, setIsVisible] = useState(true);
+    const [count, setCount] = useState(1);
+    useEffect(()=>{
+        let id= setInterval(()=>{
+            setIsVisible(c=>!c)
+        },5000)
+        return()=>{
+            clearInterval(id)
+        }
+    },[])
 
-  return (
-       <>
-       </>
-  )
+    useEffect(()=>{
+        let id = setInterval(()=>{
+            setCount(count=>count+1)
+        },1000)
+
+        return()=>{
+            clearInterval(id)
+        }
+    },[])
+
+    return (
+        <div>
+            {isVisible && <Counter value={count}/>}
+        </div>
+    )
 }
 
+function Counter({value}){
+    return <div>
+        {value}
+    </div>
+}
 export default App
