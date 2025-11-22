a9b5e563c23a424c99902cc4e161b16f1fddfd20
eccentricharry <harinathrao13@gmail.com>
2025-11-22 23:16:13 +0530

    fetch posts from source

--- React/Linkedin/src/App.jsx
+++ React/Linkedin/src/App.jsx
@@ -1,11 +1,10 @@
 import './App.css'
 import {PostComponent} from "./Post.jsx";
-import {useState} from "react";
 import posts from "./posts.js";
 
 function App() {
 
-    const [posts, setPosts] = useState(posts);
+    // const [posts, setPosts] = useState(posts);
 
 
     const postsComponents = posts.map(post =><PostComponent name={post.name}
@@ -14,21 +13,21 @@ function App() {
                                                             image={post.image}
                                                             description={post.description} />
         )
-    function addPost(){
-        setPosts([...posts, {
-            name: "Harkirat",
-            subtitle: "10,000 followers",
-            time: "2m ago",
-            image:
-                "https://appx-wsb-gcp-mcdn.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg",
-            description:
-                "Want to know how to win big? Check out how these folks won $6000 in bounties?"
-        }])
-    }
+    // function addPost(){
+    //     setPosts([...posts, {
+    //         name: "Harkirat",
+    //         subtitle: "10,000 followers",
+    //         time: "2m ago",
+    //         image:
+    //             "https://appx-wsb-gcp-mcdn.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg",
+    //         description:
+    //             "Want to know how to win big? Check out how these folks won $6000 in bounties?"
+    //     }])
+    // }
 
   return (
       <div style={{background: "#dfe6e9", height: "100vh", }}>
-          <button onClick={addPost}>Add post</button>
+          {/*<button onClick={addPost}>Add post</button>*/}
           <div style={{display: "flex", justifyContent: "center" }}>
             <div>
                 {postsComponents}
